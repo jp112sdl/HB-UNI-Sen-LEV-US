baa7990dc35e93bbd57368cbaf76f4a5e1354b0c
jp112sdl <jp112sdl@icloud.com>
2018-08-25 10:41:18 +0200

    changed calculation for SR04

--- HB-UNI-Sen-LEV-US.ino
+++ HB-UNI-Sen-LEV-US.ino
@@ -160,7 +160,7 @@ class MeasureChannel : public Channel<Hal, UList1, EmptyList, List4, PEERS_PER_C
           delayMicroseconds(10);
           digitalWrite(SENSOR_TRIG_PIN, LOW);
           m_value = pulseIn(SENSOR_ECHO_PIN, HIGH, 26000);
-          m_value = m_value / 58;
+          m_value = (m_value * 1000L / 57874L);
           digitalWrite(SENSOR_EN_PIN, LOW);
           break;
         case MAXSONAR:
