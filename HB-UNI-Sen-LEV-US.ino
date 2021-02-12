141bae5dcd5752278abaf37f5af18fdfcb4dfb09
Jérôme <jp112sdl@icloud.com>
2021-02-12 07:03:18 +0100

    Update HB-UNI-Sen-LEV-US.ino

--- HB-UNI-Sen-LEV-US.ino
+++ HB-UNI-Sen-LEV-US.ino
@@ -4,6 +4,7 @@
 // 2018-04-16 jp112sdl Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
 // 2020-03-12 Wolfram Winter Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
 //- -----------------------------------------------------------------------------------------------------------------------
+// ci-test=yes board=328p aes=no
 
 // define this to read the device id, serial and device type from bootloader section
 // #define USE_OTA_BOOTLOADER
